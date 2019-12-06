11fd5fcd1eb84cf91069f4688e1b40fd07ea1b4c
Pavel Nadolski <tbigad@gmail.com>
2019-12-06 13:51:31 +0300

    add new extentions

---
+++ Extentions/CGSizeExtention.swift
@@ -0,0 +1,39 @@
+//
+//  CGSizeExtention.swift
+//  CnnRssFeed
+//
+//  Created by Pavel N on 12/6/19.
+//  Copyright © 2019 Pavel N. All rights reserved.
+//
+
+import UIKit
+
+extension CGSize {
+    enum AspectRatio {
+        case ignore
+        case keep
+        case keepForExpandind
+    }
+    
+    func scaled(withNewSize size:CGSize, aspectRation mode:AspectRatio) -> CGSize {
+        if (mode == AspectRatio.ignore || self.width == 0 || self.height == 0) {
+            return size;
+        } else {
+            var useHeight:Bool;
+            let rw:CGFloat = CGFloat(size.height) * CGFloat(self.width) / CGFloat(self.height);
+
+            if (mode == AspectRatio.keep) {
+                useHeight = (rw <= size.width);
+            } else { // mode == AspectRatio.keepForExpandind
+                useHeight = (rw >= size.width);
+            }
+
+            if (useHeight) {
+                return CGSize(width: rw, height: size.height);
+            } else {
+                return CGSize(width: size.width,
+                              height: CGFloat(CGFloat(size.width) * CGFloat(self.height) / CGFloat(self.width)));
+            }
+        }
+    }
+}
---
+++ Extentions/UIImageView.swift
@@ -0,0 +1,23 @@
+//
+//  UIImageView.swift
+//  CnnRssFeed
+//
+//  Created by Pavel N on 12/3/19.
+//  Copyright © 2019 Pavel N. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
